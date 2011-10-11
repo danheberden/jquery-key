6a611d93aeafa31d733fd2d32f70b852508b48fa
Dan Heberden <danheberden@gmail.com>
2011-10-11 14:32:18 -0400

    Add latest jquery.key, tests, demo and readme

--- Readme.md
+++ Readme.md
@@ -1,3 +1,67 @@
 # KeyIt - a jQuery Hotkey/Combo Plugin
 
-Want to attach functions to key events? Combinations of keys? BOOM done. 
+Want to attach functions to key events? Combinations of keys? BOOM done.
+
+## bind to shift-up
+    $( '#someElement' ).key( 's38', function( event, cache, key ) {
+      // event is the original event
+      // cache is the internal object stored with $.data
+      // key is the key (or combo of keys) that fired this handler
+    });
+
+## konami code
+    $( document ).key( '38,38,40,40,37,39,37,39,66,65', function( event, cache, combo) {
+        // yay! \o/
+    });
+
+## Built-in tool to get keycodes
+Just fire `$().key( 'help' )` or `$.fn.key( 'help' )` to launch a simple
+tool to echo the appropriate key combo as you type.
+
+## Handles Mac Command/Control
+While independent mappings exist for explicit use of control or command
+(`c` and `m`) you can bind to either using a capital `C`. So `c38' will
+match control-↑, `m38` will match command-↑ and `C38` will match either
+control or command ↑.
+
+## Need to kill off a handler?
+Just rebind with false instead of a function.
+    $( document ).key( '38,38,40,40,37,39,37,39,66,65', false );
+
+## Settings
+You can pass settings as the third argument. Keep in mind, though, that
+these are attached for that particular DOM element (so that you don't
+have to pass in settings each time) - so if you change the time-out to
+300ms for an input later on, that will definitely affect attached keys
+previously attached to that same input.
+
+### bubbleInputs
+By default, events text boxes, textareas, and selects won't bubble
+up. If you want them to, pass `bubbleInputs: true` in the settings
+object.
+
+### time
+This is the time before ignoring previous keys. The default is 2000
+milliseconds.
+
+### holdTab
+Handy option to let tab work in inputs as a keypress and not actually
+tab-out of the element. This would be for snippet-like behavior, such
+as:
+    var $someTextarea = $( '#someTextarea' );
+
+    // if `f` and then tab is pressed
+    $contentBox.key( '70,9', function() {
+      var oldVal = $contentBox.val();
+      // append 'unction' to the f :)
+      $contentBox.val( oldVal + 'unction' );
+    }, {
+      holdTab: true
+    });
+
+## A note about coding style
+There's definitely some areas of the code that could be written in a
+more condensed fashion. This is on purpose. The minifier can do the
+work of condensing the code; I'd rather it be easily understood by
+everyone. I figured I'd mention this before any pull requests come
+in to make the code super-duper compact/optimized.
---
+++ src/jquery.key.js
@@ -0,0 +1,218 @@
+(function( $, document ) {
+
+  // process the keydown event
+  var handler = function( e, cache ){
+            var el = this,
+                key = e.keyCode || e.which,
+                mod = '';
+
+            // ignore control keyes (shft,cntrl,opt,cmnd,caps)
+            if ( ( key > 15 && key < 19 )  || key === 20 || key === 91 ||
+                // if we didn't ask to include inputs and didn't
+                // directly bind to the input-ish element
+                  ( !cache.options.bubbleInputs && el !== e.target &&
+                // if we're in an input-ish item ( SELect, teXTArea, iNPut )
+                  ( el.contentEditable === "true" || /(SEL|XTA|NP)/i.test( e.target.nodeName ) ) ) ) {
+                return;
+            }
+
+            // use options.time to decide when to stop caring about
+            // previous keypresses
+            var now = +new Date();
+
+            // it's been longer than our limit time since anything
+            if (  now - cache.lastKeyTime > cache.options.time ) {
+              // kill any existing cache
+              cache.keyLog = [];
+            }
+            cache.lastKeyTime = now;
+
+            // tab magic (dont tab out)
+            if ( key === 9 && cache.options.holdTab  ) {
+              e.preventDefault();
+            }
+
+            // standard mod keys
+            if ( e.altKey ) {
+              mod += 'a';
+            }
+            if ( e.ctrlKey ) {
+               mod += 'c';
+            }
+            if ( e.metaKey ) {
+              mod += 'm';
+            }
+            if ( e.shiftKey ) {
+              mod += 's';
+            }
+            mod += key;
+
+            // check for a global handler (one that gets every keypress)
+            if ( cache.actions[ '*' ] ) {
+              cache.actions[ '*' ].call( el, e, cache, mod );
+            }
+            // store into combo log
+            cache.keyLog.unshift( mod );
+
+            // truncate the array if it's too long
+            if ( cache.keyLog.length > cache.maxLength ) {
+              cache.keyLog.length = cache.maxLength;
+            }
+
+            // check the combo log - go through each key
+            for ( var i = 0; i < cache.keyLog.length; i++ ) {
+              // and for each key, build a list of permutations
+              // work backwards, since our keys are stored like that
+              for ( var j = cache.keyLog.length - 1 - i, combo = []; j >= 0; j-- ) {
+                combo.push( cache.keyLog[j] )
+              }
+              // make the string permutation
+              combo = combo.join(',');
+              // and give it a shot, yo
+              if ( cache.actions[ combo ] ) {
+                cache.actions[ combo ].call( el, e, cache, combo );
+              }
+            }
+
+        },
+
+        // default options
+        defaultOptions =  {
+          // time until keypresses get forgotten
+          time: 2000,
+          // collect events from inputs on non-inputs/directly bound
+          // events
+          bubbleInputs: false,
+          // if user presses tab in input, don't lose focus
+          holdTab: false
+        },
+
+        // Add key->action to _key_actions object via $.data
+        // wrap the keyup handler to use the _key_cache object
+        bindAction = function( key, action, options ) {
+          var $this = $( this ),
+              data = $this.data() || {},
+              cache,
+              keyLength = key.split(',').length;
+
+          // make our default object to hold everything
+          // that's associated with the dom node bound
+          if ( !data._key_cache ) {
+            data._key_cache = {
+              actions: {},
+              handler: undefined,
+              keyLog: [],
+              maxLength: 0,
+              lastKeyTime: 0,
+              options: $.extend( {}, defaultOptions )
+            };
+          }
+          cache = data._key_cache;
+
+          // mesh in the custom options with this node's optinos
+          $.extend( cache.options, options );
+
+          // bind up the action to the key
+          cache.actions[ key ] = action;
+
+          // make sure our maxLength buffer is updated
+          if ( keyLength > cache.maxLength ) {
+            cache.maxLength = keyLength;
+          }
+
+          // we only want one handler bound to this element
+          // since it's the same actions obj, future el:key->action
+          // binds will work
+          if ( !cache.handler ) {
+            // make the handler with a wrapper fn to preserve
+            // the actions and log in scope
+            cache.handler = function(e){
+              handler.call( $this[0], e, cache );
+            }
+
+            // actually bind/make the handler
+            $this.keydown( cache.handler );
+          }
+
+          return this;
+        },
+
+        // preprocessor for using C 'n stuff
+        addKey = function( keys, action, opts ) {
+           var controlCopy = keys,
+              metaCopy,
+              $this = $( this ),
+              data = $this.data();
+
+          // handle capital C
+          // if there's a capital C, we need to make one with `c` and one with `m`
+          // as of now, not supporting permutations of them for useability sake
+          if ( /C/.test( keys ) ) {
+            controlCopy = keys.replace( '/C/g', 'c' );
+            metaCopy = keys.replace( '/C/g', 'm' );
+          }
+
+          //bind up control to this
+          bindAction.call( this, controlCopy, action, opts);
+
+          // wanna bind some meta?
+          if ( metaCopy ) {
+            bindAction.call( this, metaCopy, action, opts );
+          }
+        },
+
+        // Just a little thing to record the combo you want -
+        // run $.fn.key( 'help' ) or $().key('help') to launch the
+        // dialog/popup to track keyinputs
+        help = function() {
+          // add content to dom
+          var $input = $('<input style="width:100%;border:1px solid #CCC;font-size:1.3em;">'),
+              $output = $( '<div>' ),
+              $clear = $( '<button>CLEAR</button>'),
+              $halp = $( '<div style="position:absolute;top:0;left:0;width:100%;z-index:9001;background:#000;color:#FFF;font-size:1.3em;">' ).append( $input, $clear, $output );
+
+          // bind input log
+          $input.key('*', function( e, cache, key ) {
+              var prev = $output.html();
+              $output.html( prev + ( prev.length ? ',': '' ) + key );
+            }, {
+              keepTabInInput: true
+            });
+
+          // clear button
+          $clear.bind( 'click', function() {
+            $output.html('');
+            $input.val('');
+          });
+
+          // oh harro dom
+          $( document.body ).append( $halp );
+        };
+
+        // make the actual plugin
+        $.fn.key = function( keys, action, opts ) {
+          // asking for the combo maker tool thingy?
+          if ( keys === "help" ) {
+            help();
+            return false;
+          }
+
+          var _this = this;
+
+          // make keys an object if one wasn't passed
+          if ( $.type( keys ) !== 'object' ) {
+            var newKeys = {};
+            newKeys[ keys ] = action;
+            keys = newKeys;
+          } else {
+            opts = action;
+          }
+        // for each key-set, bind it up
+          $.each( keys, function( i, v ) {
+            addKey.call( _this, i, v, opts );
+          });
+
+        // chain this shit, yo
+        return this;
+      };
+})( jQuery, document );
